cfd3ca7443365bc1dd1e066706431e3d32e6f55f
Nasirianfar <mohamadrezanasirianfar@gmail.com>
2024-10-30 20:22:42 +0330

    update request model

--- internal/domain/models/request.go
+++ internal/domain/models/request.go
@@ -4,8 +4,8 @@ import "time"
 
 // Request represents a user request.
 type Request struct {
-	ID         string    // Unique identifier
-	UserID     string    // ID of the user making the request
-	Data       string    // The request data
-	ReceivedAt time.Time // Time the request was received
+    ID         string    `json:"id"`
+    UserID     string    `json:"user_id"`
+    Data       string    `json:"data"`
+    ReceivedAt time.Time `json:"received_at"`
 }
